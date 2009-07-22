4c309cca115f78d18d94c843ecd64b3ff967a165
kugg <kugg@farjestad.(none)>
2009-07-22 19:45:48 +0200

    Added sinus wave generator for testing single sinus waves.

--- applet/dtmf.cpp
+++ applet/dtmf.cpp
@@ -1,3 +1,4 @@
+// References
 //========DTMGF table========
 //     1209 	1336  	1477   1633
 //697  1        2       3      A
@@ -5,15 +6,28 @@
 //852  7        8  	9      C
 //941  * 	0 	#      D
 //============================
-#define PIN_ROW 9
-#define PIN_COL 10
+//http://www.ocf.berkeley.edu/~fricke/dtmf.html
+//http://en.wikipedia.org/wiki/Dtmf
+//http://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-F.902-199502-I!!PDF-E&type=items
 
-#define MAX_BINS 4          
+
+#define PIN_ROW 9
+#define SOUNDOUT_PIN 9
+
+
+#define MAX_BINS 4      
+
 #include "WProgram.h"
 void setup();
+int write_signal(int col_freq, int row_freq, int length);
 struct retarr keytofreq(char key);
-int freqtoperiod (int freq);
 void loop();
+void Sound(float freqcol, float freqrow, int durationMS, int outputPin);
+int period(int freq1,int freq2,int durationMS);
+static double pi = 3.14159;
+double samprate = 8000.0;      /* 8000 samples per second */
+
+
 int      freqs_row[ MAX_BINS] = 
 {
   697,
@@ -47,9 +61,21 @@ void setup()
   Serial.begin(9600);
  // pinMode(PIN_ROW, OUTPUT);
  // pinMode(PIN_COL, OUTPUT);
-
-  
 }
+
+int write_signal(int col_freq, int row_freq, int length) {
+  int tone_length=1;
+  unsigned int sample;
+  while (tone_length<length) {
+/*    sample = (unsigned char)(128.0 + 64.0*cos(2*pi*col_freq*tone_length) 
+                                + 64.0*cos(2*pi*row_freq*tone_length));*/
+    sample = ( cos( 2*pi*col_freq*tone_length ) + cos( 2*pi*row_freq*tone_length ) );
+    analogWrite(PIN_ROW, sample);
+    tone_length += 1.0; /* increment time */
+  }
+  return(0);
+}
+
 
 struct retarr keytofreq(char key)
 {
@@ -66,32 +92,84 @@ struct retarr keytofreq(char key)
     }
   }
 }
-
+/*
 int freqtoperiod (int freq) {
-  
+  analogWrite(PIN_ROW, 75);
   int period;
   return period;
 }
+*/
+
 
 void loop()
 {
-  analogWrite(PIN_ROW, 20);
- // analogWrite(PIN_COL, 200);
 
-  delay(1000);
+  Sound(697,1209,1000,9); 
+  Sound(770,1336,1000,9);
+  Sound(852,1477,1000,9);
+  Sound(941,1633,1000,9);
+
+  // analogWrite(PIN_ROW, 20);
+ // analogWrite(PIN_COL, 200);
  
   struct retarr pfreq1 = keytofreq('1');
   struct retarr pfreq2 = keytofreq('2');
   struct retarr pfreq3 = keytofreq('3');
-
-  Serial.println(pfreq1.row_col[0]);
-  Serial.println(pfreq1.row_col[1]);
+  Serial.println("Tone 1:");
+  write_signal(pfreq1.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
   
-  analogWrite(PIN_ROW, 75);
-//  analogWrite(PIN_COL, 57);
+  Serial.println("Tone 2:");
+  write_signal(pfreq2.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
+  
+  Serial.println("Tone 3:");
+  write_signal(pfreq3.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
+  //Serial.println(pfreq1.row_col[0]);
+  //Serial.println(pfreq1.row_col[1]);
+  
+  
+    //analogWrite(PIN_ROW, 254);
   delay(500);
 }
 
+void Sound(float freqcol, float freqrow, int durationMS, int outputPin) {
+  int halfPeriod;
+  float period;
+  int durationCycles;
+  //Check for rest, 0 frequency is a rest for durationMS.
+  if(freqrow==0.0) {
+    //0 frequency so we stay quiet for duration
+    delay (durationMS);
+  }
+  else { //Frequency is not zero so we have work to do
+    // turn on output pin
+    pinMode(outputPin, OUTPUT);
+    //calculate the period or cycle time for the given frequency
+    period=1/freqrow; //Take the reciprocal to get time in seconds
+    period=period*1.0E6; //to covert seconds to uS.
+    //divide that by 2 to get the 1/2 cycle time. convert to int at the same time
+    halfPeriod = (int)(period/2.0) - 7; // subtract 7 us to make up for digitalWrite overhead
+    
+    // calculate cycles for duration.
+    durationCycles = (int)(((float)durationMS*1000.0)/period); // play note for duration ms
+    
+    for (int i=0; i<durationCycles; i++){
+      digitalWrite(outputPin, HIGH);
+      delayMicroseconds(halfPeriod);
+      digitalWrite(outputPin, LOW);
+      delayMicroseconds(halfPeriod - 1); // - 1 to make up for fractional microsecond        in digitaWrite overhead
+    }
+    // shut off pin to avoid noise from other operations
+    pinMode(outputPin, INPUT);
+  }
+}
+
+int period(int freq1,int freq2,int durationMS) {
+  int length;
+  return(length);
+}
 int main(void)
 {
 	init();
--- applet/dtmf.pde
+++ applet/dtmf.pde
@@ -1,3 +1,4 @@
+// References
 //========DTMGF table========
 //     1209 	1336  	1477   1633
 //697  1        2       3      A
@@ -5,10 +6,19 @@
 //852  7        8  	9      C
 //941  * 	0 	#      D
 //============================
-#define PIN_ROW 13
-#define PIN_COL 12
+//http://www.ocf.berkeley.edu/~fricke/dtmf.html
+//http://en.wikipedia.org/wiki/Dtmf
+//http://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-F.902-199502-I!!PDF-E&type=items
 
-#define MAX_BINS 4          
+
+#define PIN_ROW 9
+
+#define MAX_BINS 4      
+
+static double pi = 3.14159;
+double samprate = 8000.0;      /* 8000 samples per second */
+
+
 int      freqs_row[ MAX_BINS] = 
 {
   697,
@@ -42,9 +52,21 @@ void setup()
   Serial.begin(9600);
  // pinMode(PIN_ROW, OUTPUT);
  // pinMode(PIN_COL, OUTPUT);
-
-  
 }
+
+int write_signal(int col_freq, int row_freq, int length) {
+  int tone_length=1;
+  unsigned int sample;
+  while (tone_length<length) {
+/*    sample = (unsigned char)(128.0 + 64.0*cos(2*pi*col_freq*tone_length) 
+                                + 64.0*cos(2*pi*row_freq*tone_length));*/
+    sample = ( cos( 2*pi*col_freq*tone_length ) + cos( 2*pi*row_freq*tone_length ) );
+    analogWrite(PIN_ROW, sample);
+    tone_length += 1.0; /* increment time */
+  }
+  return(0);
+}
+
 
 struct retarr keytofreq(char key)
 {
@@ -61,28 +83,38 @@ struct retarr keytofreq(char key)
     }
   }
 }
-
+/*
 int freqtoperiod (int freq) {
-  
+  analogWrite(PIN_ROW, 75);
   int period;
   return period;
 }
+*/
+
 
 void loop()
 {
-  analogWrite(PIN_ROW, 20);
+ // analogWrite(PIN_ROW, 20);
  // analogWrite(PIN_COL, 200);
-
-  delay(1000);
  
   struct retarr pfreq1 = keytofreq('1');
   struct retarr pfreq2 = keytofreq('2');
   struct retarr pfreq3 = keytofreq('3');
-
-  Serial.println(pfreq1.row_col[0]);
-  Serial.println(pfreq1.row_col[1]);
+  Serial.println("Tone 1:");
+  write_signal(pfreq1.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
   
-  analogWrite(PIN_ROW, 75);
-//  analogWrite(PIN_COL, 57);
+  Serial.println("Tone 2:");
+  write_signal(pfreq2.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
+  
+  Serial.println("Tone 3:");
+  write_signal(pfreq3.row_col[0],pfreq1.row_col[1],10000);
+  delay(1000);
+  //Serial.println(pfreq1.row_col[0]);
+  //Serial.println(pfreq1.row_col[1]);
+  
+  
+    //analogWrite(PIN_ROW, 254);
   delay(500);
 }
